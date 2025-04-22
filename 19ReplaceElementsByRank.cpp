43da71f6e40633966d2526058fac3e532b18fc5d
Student ApnaCollege <gauravbaghel629@gmail.com>
2025-04-22 23:32:02 +0530

    Solved Replace Elments by Ranks

---
+++ 19ReplaceElementsByRank.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n = 6;
+    int arr[n] = {20,15,26,2,98,6};
+
+    map<int,int> mpp;
+
+    int temp = 1;
+    int brr[n];
+
+    for(int i=0;i<n;i++){
+        brr[i] = arr[i];
+    }
+
+
+    sort(brr, brr + n);
+    for(int i=0;i<n;i++){
+        if(mpp.find(brr[i]) == mpp.end()){
+            mpp[brr[i]] = temp;
+            temp++;
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        cout<<mpp[arr[i]]<<" ";
+    }
+
+    return 0;
+}
