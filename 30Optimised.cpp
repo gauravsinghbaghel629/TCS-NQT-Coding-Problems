52019eb4cfa0a664b577c7192d9dd38dba38d8c3
Student ApnaCollege <gauravbaghel629@gmail.com>
2025-04-30 01:20:15 +0530

    Optimised it further

---
+++ 30Optimised.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPerfect(int n){
+    int sum = 0;
+
+    for(int i=1;i*i<=n;i++){
+        if(n % i == 0){
+            if(i*i == n || i == 1){
+                sum += i;
+            }
+            else{
+                sum += i + n/i;
+            }
+        }
+    }
+
+    return (sum == n);
+}
+
+int main(){
+    bool ex1 = isPerfect(6);
+    bool ex2 = isPerfect(15);
+    bool ex3 = isPerfect(28);
+
+    if (ex1 == true) {
+        cout << "6 is a perfect number" << endl;
+    } else cout << "6 is not a perfect number" << endl;
+
+    if (ex2 == true) {
+        cout << "15 is a perfect number" << endl;
+    } else cout << "15 is not a perfect number" << endl;
+
+    if (ex3 == true) {
+        cout << "28 is a perfect number" << endl;
+    } else cout << "28 is not a perfect number" << endl;
+
+    return 0;
+}
