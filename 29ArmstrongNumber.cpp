035f44266a9c7f11cced522d76a3a957607b394a
Student ApnaCollege <gauravbaghel629@gmail.com>
2025-04-30 01:09:21 +0530

    Upgraded the code

--- 29ArmstrongNumber.cpp
+++ 29ArmstrongNumber.cpp
@@ -1,34 +1,20 @@
-#include <bits/stdc++.h> 
+#include<bits/stdc++.h> 
 using namespace std;
 
-// Function to check if a
-// number is an Armstrong number
-bool isArmstrong(int num) {
-    // Calculate the number of
-    // digits in the given number
-    int k = to_string(num).length();
-    // Initialize the sum of digits
-    // raised to the power of k to 0
+bool isArmstrong(int n){
+    int len = to_string(n).length();
+
     int sum = 0;
-    // Copy the value of the input
-    // number to a temporary variable n
-    int n = num;
-    // Iterate through each
-    // digit of the number
+    int m = n;
+
     while(n > 0){
-        // Extract the last
-        // digit of the number
-        int ld = n % 10;
-        // Add the digit raised to
-        // the power of k to the sum
-        sum += pow(ld, k); 
-        // Remove the last digit
-        // from the number
+        int digit = n % 10;
+
+        sum += pow(digit,len);
+
         n = n / 10;
     }
-    // Check if the sum of digits raised to
-    // the power of k equals the original number
-    return sum == num ? true : false;
+    return (sum == m);
 }
 
 int main() {
