4d2697b740a6640248434293da84d8a841c2ef9e
Student ApnaCollege <gauravbaghel629@gmail.com>
2025-05-02 01:51:11 +0530

    Uploaded whether even or odd

---
+++ 31IsEven_OR_ODD.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(int n){
+    if(n%2 == 0){
+        return 0;
+    }
+    else return 1;
+}
+
+int main() {
+    int n = 5;
+    if (solve(n) == 0) {
+      cout<<n<<" is even.";
+    } else {
+      cout<<n << " is odd.";
+    }
+  }
