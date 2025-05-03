c09f574b1aa049ec5c7201aafe9a01f2a83ce464
Student ApnaCollege <gauravbaghel629@gmail.com>
2025-05-03 23:55:17 +0530

    Solved sum of AP

---
+++ 34SumOfAP.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+float sumofAp(float a,float d,float n){
+    float ans = n / 2.0;
+    ans = ans * (2.0 * a + (n-1) * d);
+
+    return ans;
+}
+
+int main(){
+    float a = 1.5, d = 3;
+    int n = 5;
+    cout << "Sum of Given Ap Series: " << sumofAp(a, d, n) << endl;
+}
