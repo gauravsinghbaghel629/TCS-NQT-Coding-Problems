f2e5c98da0979bee8459aaa149fe196183b0f90d
Student ApnaCollege <gauravbaghel629@gmail.com>
2025-05-08 23:36:22 +0530

    Solved using if-else condition

---
+++ 36_optimised.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    double n1 = 1.123, n2 = 1.124;
+
+    if(n1 < n2){
+        cout<<"The greatest of the two no.'s is: "<<n2;
+    }
+    else{
+        cout<<"The greatest of the two no.'s is: "<<n1;
+    }
+
+    return 0;
+}
