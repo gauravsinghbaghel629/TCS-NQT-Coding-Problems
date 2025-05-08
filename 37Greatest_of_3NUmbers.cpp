1855bc89108a223f4c39eefeab4f5354698b76d3
Student ApnaCollege <gauravbaghel629@gmail.com>
2025-05-08 23:39:13 +0530

    Solved using max func

---
+++ 37Greatest_of_3NUmbers.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    double n1 = 1, n2 = 3, n3 = 5;
+
+    cout<<"The maximum of the three numbers is: "<<max(n1,(n2,n3));
+
+    return 0;
+}
