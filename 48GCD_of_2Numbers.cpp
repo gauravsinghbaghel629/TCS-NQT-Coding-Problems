4a5d63eb1323de4dec46c14fa73c57740c6ae58f
Student ApnaCollege <gauravbaghel629@gmail.com>
2025-05-18 00:09:05 +0530

    Updated the code

--- 48GCD_of_2Numbers.cpp
+++ 48GCD_of_2Numbers.cpp
@@ -5,23 +5,14 @@
 using namespace std;
 
 int findGcd(int n1, int n2) {
-    // Initialize gcd to 1
     int gcd = 1;
 
-    // Iterate from 1 up to
-    // the minimum of n1 and n2
-    for(int i = 1; i <= min(n1, n2); i++) {
-        // Check if i is a common
-        // factor of both n1 and n2
-        if(n1 % i == 0 && n2 % i == 0) {
-            // Update gcd to the
-            // current common factor i
+    for(int i=1;i<=max(n1,n2);i++){
+        if(n1 % i == 0 && n2 % i == 0){
             gcd = i;
         }
     }
 
-    // Return the greatest
-    // common divisor (gcd)
     return gcd;
 }
 
