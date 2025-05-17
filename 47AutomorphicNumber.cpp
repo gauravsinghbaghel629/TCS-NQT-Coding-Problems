1ec1499dfa24a2e2cc97615dec931c2e4aafe572
Student ApnaCollege <gauravbaghel629@gmail.com>
2025-05-18 00:06:01 +0530

    Updated the code

--- 47AutomorphicNumber.cpp
+++ 47AutomorphicNumber.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
-
 using namespace std;
 
-bool isAutomorphic(int N) {
+bool isAutomorphic(int n){
+    int sq = n * n;
 
-  int sq = N * N;
+    while(n != 0){
+        if(n % 10 != sq % 10){
+            return false;
+        }
 
-  while (N > 0) {
-
-    // Check if last digit is equal or not
-    if (N % 10 != sq % 10)
-      return false;
-
-    // Reducing the number and its square
-    N /= 10;
-    sq /= 10;
-  }
-
-  return true;
+        n = n / 10;
+        sq = sq / 10;
+    }
+    return true;
 }
+
 
 int main() {
   int N = 25;
