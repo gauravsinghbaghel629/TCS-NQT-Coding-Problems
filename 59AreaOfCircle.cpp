eed270bab7a9fc6675ffb50181f00d45826c36e8
Student ApnaCollege <gauravbaghel629@gmail.com>
2025-05-26 23:53:06 +0530

    Solved area of circle

---
+++ 59AreaOfCircle.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void areaOfCircle(int n){
+    double ans = 3.14 * n * n;
+    cout<<"Area of Circle is: "<<ans<<endl;
+    return;
+}
+
+int main(){
+    int n = 5;
+    
+    areaOfCircle(n);
+    return 0;
+}
